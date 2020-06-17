ca903696e0ab4dc52c361dabe043d5a37d14a2c3
yintii <kheart@keleheart.com>
2020-06-17 15:55:46 -0500

    working on implementing JS to start the process of changing the HTML content of the divs

---
+++ game.js
@@ -0,0 +1,21 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const computerPicks = [rock, paper, scissors];
+let score = 0;
+
+function playerThrow(){
+
+}
+
+function computerThrow(){
+
+}
+
+function compareChoices(){
+
+}
+
+function updateScore(){
+    
+}
